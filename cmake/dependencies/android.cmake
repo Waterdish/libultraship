1799c5f863ada0bb222afb37f613c9877b5e2648
Robert Kirkman <misternumberone@live.com>
2025-02-14 17:56:49 -0600

    special external storage (2s2h)
    - bump SDL to 2.32.0
    - drop tinyxml2 to https://github.com/leethomason/tinyxml2/commit/a9773976845b19e89020c1215781e71116477ef1
    - force `SDL_SHARED_ENABLED_BY_DEFAULT`
    - enable spdlog logcat backend
    - add Special External Storage Permission support

--- cmake/dependencies/android.cmake
+++ cmake/dependencies/android.cmake
@@ -3,10 +3,13 @@ include(FetchContent)
 #=================== SDL2 ===================
 find_package(SDL2 QUIET)
 if (NOT ${SDL2_FOUND})
+    # if SDL_SHARED_ENABLED_BY_DEFAULT unset, for me libSDL2.so entirely failed to build and link, causing a vague runtime error:
+    # java.lang.NullPointerException: Attempt to invoke virtual method 'void android.view.ViewGroup.addView(android.view.View)' on a null object reference
+    set(SDL_SHARED_ENABLED_BY_DEFAULT ON)
     FetchContent_Declare(
         SDL2
         GIT_REPOSITORY https://github.com/libsdl-org/SDL.git
-        GIT_TAG release-2.30.5
+        GIT_TAG release-2.32.0
         OVERRIDE_FIND_PACKAGE
     )
     FetchContent_MakeAvailable(SDL2)
@@ -31,7 +34,14 @@ if (NOT ${tinyxml2_FOUND})
     FetchContent_Declare(
         tinyxml2
         GIT_REPOSITORY https://github.com/leethomason/tinyxml2.git
-        GIT_TAG 10.0.0
+        # last currently good commit of tinyxml2 at time of writing for 32-bit android 5 & 6
+        # see discussion here
+        # https://github.com/leethomason/tinyxml2/pull/945
+        # https://github.com/leethomason/tinyxml2/pull/973
+        # 32-bit bionic libc documentation and test code sample for targeting 32-bit android 5 & 6
+        # is helpful for debugging this and identifying good commits:
+        # https://android.googlesource.com/platform/bionic/+/master/docs/32-bit-abi.md
+        GIT_TAG a977397
         OVERRIDE_FIND_PACKAGE
     )
     FetchContent_MakeAvailable(tinyxml2)
